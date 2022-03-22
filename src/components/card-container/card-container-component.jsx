df65fb2e9b1fc531317c72002949f29911856b9b
mariyanailieva <mariyanailianilieva@gmail.com>
2022-03-22 12:33:40 +0200

    Monster Card

---
+++ src/components/card-container/card-container-component.jsx
@@ -0,0 +1,19 @@
+import { Component } from "react";
+import './card-container-styles.css'
+
+class CardContainer extends Component {
+    render(){
+        const {id,email,name} = this.props.monster
+        return(
+            <div className="card-container" key={id}>
+            <img
+              alt={`monster ${name}`}
+              src={`https://robohash.org/${id}?set=set2&size=180x180`}
+            />
+            <h2>{name}</h2>
+            <p>{email}</p>
+          </div>
+        )
+    }
+}
+export default CardContainer;
